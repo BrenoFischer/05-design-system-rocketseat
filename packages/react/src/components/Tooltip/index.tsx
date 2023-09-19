a396496ca4a1f08e9bc21141ca28d63cb7c255ab
Breno Fischer <breno_perricone@hotmail.com>
2023-09-19 08:02:54 -0300

    add Tooltip component + storie

---
+++ packages/react/src/components/Tooltip/index.tsx
@@ -0,0 +1,26 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+import { TooltipContent } from './styles'
+
+export interface TooltipProps {
+  children: React.ReactNode
+  content: string | React.ReactNode
+}
+
+export function Tooltip({ children, content, ...props }: TooltipProps) {
+  return (
+    <TooltipPrimitive.Provider>
+      <TooltipPrimitive.Root {...props}>
+        <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Portal>
+          <TooltipContent>
+            <TooltipPrimitive.Arrow />
+
+            {content}
+          </TooltipContent>
+        </TooltipPrimitive.Portal>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
+  )
+}
+
+Tooltip.displayName = 'Tooltip'
